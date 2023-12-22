ec8f7ddb2e65b19e3cac42ff6fc733faae823f0e
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2023-12-22 19:39:17 +0800

    udpate domain department

--- practice-go-db/internal/core/domain/department.go
+++ practice-go-db/internal/core/domain/department.go
@@ -1,7 +1,12 @@
 package domain
 
+import "encoding/json"
 
 type Department struct {
-	Id int64
-	Title string
+	Id    int64  `json:"id"`
+	Title string `json:"title"`
+}
+
+func (d Department) MarshalBinary() ([]byte, error) {
+	return json.Marshal(d)
 }
