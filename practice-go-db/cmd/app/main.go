86cf1bb559636835ea675312db83642f17c53a51
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2023-12-22 19:38:06 +0800

    Update main.go

--- practice-go-db/cmd/app/main.go
+++ practice-go-db/cmd/app/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"context"
+
+	// "time"
+
 	"fmt"
-	"go-db/internal/core/dto"
+	"go-db/internal/core/service"
 	"go-db/internal/data/repository"
+	"go-db/internal/http/api"
+	"go-db/internal/http/hendler"
+	"time"
+
 	"go-db/pkg/config"
 	"go-db/pkg/psql"
 )
@@ -13,19 +19,21 @@ func main() {
 	if err := config.LoadEnv(".env"); err != nil {
 		panic(err)
 	}
+
 	cfg := config.NewConfig()
-	dbm, err := psql.NewDbManager(cfg.GetConnectionUrl(), cfg.GetMaxConnections())
+
+	pgClient, err := psql.NewDbManager(cfg.GetConnectionUrl(), cfg.GetMaxConnections())
 	if err != nil {
 		panic(err)
 	}
 
-	dRepository := repository.NewDepartmentRepository(dbm)
-	res, err := dRepository.AddDepartment(context.Background(), &dto.AddDepartmentDTO{
-		Title: "IT",
-	})
-	if err != nil {
+	departmentRepository := repository.NewDepartmentRepository(pgClient)
+	departmentServcie := service.NewDepartmentService(departmentRepository)
+	departmentHendler := hendler.NewDepartmentHendler(departmentServcie, time.Duration(10 * time.Second))
+	api := api.NewApi(cfg.GetHttpPort(), departmentHendler)
+
+	fmt.Println("server works on :", cfg.GetHttpPort())
+	if err := api.Run(); err != nil {
 		panic(err)
 	}
-
-	fmt.Println(*res)
 }
