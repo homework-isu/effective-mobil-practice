e501063abc773a62b9506dfcd4b1489bb959231b
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2023-12-22 19:39:48 +0800

    update dto department

--- practice-go-db/internal/core/dto/department.go
+++ practice-go-db/internal/core/dto/department.go
@@ -1,6 +1,14 @@
 package dto
 
-
 type AddDepartmentDTO struct {
-	Title string
+	Title string `json:"title"`
+}
+
+type IdDerartmentDTO struct {
+	Id int64 `json:"id"`
+}
+
+type RenameDerartmentDTO struct {
+	Id       int64  `json:"id"`
+	NewTitle string `json:"new_title"`
 }
