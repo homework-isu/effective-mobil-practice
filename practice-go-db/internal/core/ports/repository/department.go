179cbad6c84a7d7db7e25ce47e316a05a0af508a
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2023-12-22 19:40:28 +0800

    add repositody interface

---
+++ practice-go-db/internal/core/ports/repository/department.go
@@ -0,0 +1,16 @@
+package repository
+
+import (
+	"context"
+	"go-db/internal/core/domain"
+	"go-db/internal/core/dto"
+)
+
+type DepartmentRepository interface {
+	AddDepartment(context.Context, *dto.AddDepartmentDTO) (*domain.Department, error)
+	DeleteDepartment(context.Context, *dto.IdDerartmentDTO) error
+	GetDepartmentById(context.Context, *dto.IdDerartmentDTO) (*domain.Department, error)
+	GetDepartments(context.Context, *dto.LimitOffsetDTO) ([]domain.Department, error)
+	RenameDepartment(context.Context, *dto.RenameDerartmentDTO) (*domain.Department, error)
+	RenameSomeDepartments(context.Context, []dto.RenameDerartmentDTO) ([]domain.Department, error)
+}
